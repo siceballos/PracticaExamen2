5b6d92626782f4f5f5bdb78c1e975f88c0ebd7a0
siceballos <si.ceballos@duocuc.cl>
2026-07-14 10:33:20 -0400

    entrega de ETF

---
+++ Automotora.py
@@ -0,0 +1,35 @@
+def agregar(automotora):
+
+
+automotora=[]
+while True:
+    print("MENU AUTOMOTORA")
+    print("1.- Agregar Vehiculo")
+    print("2.- Mostrar un Vehiculo")
+    print("3.- Eliminar Vehiculo")
+    print("4.- Listar Taller")
+    print("5.- Valorizar Taller")
+    print("6.- Salir")
+    opc=int(input("Ingrese su opción :"))
+    if(opc==1):
+         print()
+    elif(opc==2):
+        print()
+    elif(opc==3):
+        print()
+    elif(opc==4):
+        print()
+    elif(opc==5):
+        print()
+    elif(opc==6):
+        print("Fin de programa")
+        break
+ 
+
+
+
+
+
+
+
+
